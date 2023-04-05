0d54caeefd3cc8f18626c9cba2876fa0cbc804c9
ddilarakarakas <karakasdilara1@gmail.com>
2023-04-05 20:56:19 +0300

    Initial Commit

---
+++ MyOperatingSystem/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MyOperatingSystem
+//
+//  Created by Dilara Karakaş on 5.04.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
